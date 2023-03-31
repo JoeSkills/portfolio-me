12c247713be958f0169a5c545859a191b0bf9040
Joseph Oritseweyinmi <josephoritseweyinmi5@gmail.com>
2023-03-31 08:56:54 +0100

    changed some text

--- components/About.jsx
+++ components/About.jsx
@@ -5,7 +5,7 @@ import myImg from '../public/Ai-Avatar.png';
 const About = () => {
   return (
     <div className="about-section" id="About Me">
-      <span className="heading about">About</span>
+      <span className="heading about">Who Am I?🤔</span>
       <div className="about-body">
         I started my coding journey 4 years ago. I'm naturally curious🤔 and I'm
         always improving my web design skills. In my spare time I love watching
