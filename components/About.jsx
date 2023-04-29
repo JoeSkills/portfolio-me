ce40c2466c620a0e7c63b53b371d68ab5ff3ca31
Joseph Oritseweyinmi <josephoritseweyinmi5@gmail.com>
2023-04-29 01:03:51 +0100

    changed some css

--- components/About.jsx
+++ components/About.jsx
@@ -6,20 +6,25 @@ const About = () => {
   return (
     <div className="about-section section-spacing" id="About Me">
       <span className="heading about">Who Am I ?🤔</span>
-      <div className="about-body">
-        I started my coding journey 4 years ago. I'm naturally curious🤔 and I'm
-        always improving my web design skills. In my spare time I love watching
-        movies🎥, playing games🎮 and building my own projects. I have
-        experience on both frontend and backend technologies. My preferred tools
-        are NodeJs, React and MongoDB.
+      <div className="about-section__wrapper">
+        <div className="about-body">
+          I started my coding journey 4 years ago. I'm naturally curious🤔 and
+          I'm always improving my web design skills. In my spare time I love
+          watching movies🎥, playing games🎮 and building my own projects. I
+          have experience on both frontend and backend technologies. My
+          preferred tools are NodeJs, React and MongoDB.
+        </div>
+
+        <div className="about-section__img-wrapper">
+          <Image src={myImg} alt="my image" className="my-img about" />
+        </div>
       </div>
-      <Image src={myImg} alt="my image" className="my-img about" />
-      <a
+      <button
         href="https://github.com/JoeSkills"
         className="btn btn--green about-btn-github"
       >
         Check Me Out On Github
-      </a>
+      </button>
     </div>
   );
 };
